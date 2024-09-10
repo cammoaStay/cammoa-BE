3b83bd64527f30bb652d4fa460084ba42365286d
owencity <skyko6530@gmail.com>
2024-09-10 16:29:59 +0900

    project start

---
+++ src/main/java/com/cammoastay/zzon/ZzonApplication.java
@@ -0,0 +1,13 @@
+package com.cammoastay.zzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cammoastay/zzon/ZzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cammoastay.zzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
