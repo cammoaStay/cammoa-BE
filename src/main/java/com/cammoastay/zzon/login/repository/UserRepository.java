8e02195e13ac832ddf4a723ba74eecb6c2ed9f96
Kimdongkyu <151708233+owencity@users.noreply.github.com>
2024-09-23 19:39:09 +0900

    Delete src/main/java/com/cammoastay/zzon/login directory

--- src/main/java/com/cammoastay/zzon/login/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cammoastay.zzon.login.repository;
-
-
-import com.cammoastay.zzon.login.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-	UserEntity findByUsername(String username);
-}
